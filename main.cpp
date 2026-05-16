3d6124e4a7ebc5cc9aa3cd804373ccd8fddda5c8
Caden-9 <cjw916@gmail.com>
2026-05-16 01:08:13 -0400

    Drawing testing: parabola; set tick speed to 60

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 #include <SFML/Window.hpp>
 using namespace sf;
 
+//Flight information
+float x = 0, x_old, y = 0, y_old, v = 1000, a = -500, dt = 1.f/60.f;
+
 int main()
 {
     //Window
-    RenderWindow window(VideoMode({2000, 1500}), "RTS", Style::Titlebar | Style::Close);
+    RenderWindow window(VideoMode({1920, 1080}), "RTS", Style::Titlebar | Style::Close);
+    window.setFramerateLimit(60);
+    //Draw black screen
+    window.clear(Color::Black); 
 
     //Simulator Loop
     while(window.isOpen())
@@ -20,14 +26,20 @@ int main()
                 window.close();
         }
 
-        //Update state
-        
+        //Update position
+        x_old = x;
+        y_old = y;
+        y += v*dt;
+        v += a*dt;
+        x += 250.f*dt;
 
-        //Draw Screen
-
-        window.clear(Color::Black); //Clear old
+            //Draw Screen
         //Draw new
-
+        Vertex line[] = {
+            Vertex{{x_old, 1080.f - y_old}},
+            Vertex{{x, 1080.f - y}}
+        };
+        window.draw(line, 2, sf::PrimitiveType::Lines);
 
         //Display new
         window.display();
