7ef3b519a62ce94c0ebd8a13717aecdd7d576b00
Caden-9 <cjw916@gmail.com>
2026-05-19 16:02:09 -0400

    Added acceleration, velocity, and position functions for x and y and added fuel mass that changes with time.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,26 @@
 #include <SFML/Window.hpp>
 using namespace sf;
 
-//Functions
+    /*Functions*/
+
+//Menu and Simulator
 void menu(RenderWindow& window);
 void simulator(RenderWindow& window);
+
+/*Calculations*/
+
+//Physics
+float acceleration_y(float t);
+float velocity_y(float t);
+float position_y(float t);
+float acceleration_x(float t);
+float velocity_x(float t);
+float position_x(float t);
+
 void calculate_bounds();
 void update_numbers();
+
+/*Screen*/
 void draw_path(RenderWindow& window);
 void draw_graph(RenderWindow& window, RectangleShape& x_axis, RectangleShape& y_axis, RectangleShape& ticks);
 void draw_state(RenderWindow& window, Text& time, Text& distance, Text& height, Text& velocity, Text& acceleration);
@@ -20,9 +35,7 @@ void draw_arrows(RenderWindow& window);
 
 //Constants
 constexpr float GRAVITY = -9.81,
-                FORCE = 400, MASS = 5,
                 PI = 3.14159265,
-                THRUST_TIME = 1.5,
                 ORIGIN = 50,
                 X_AXIS_LENGTH = 1600, Y_AXIS_LENGTH = 900;
 
@@ -30,11 +43,14 @@ constexpr float GRAVITY = -9.81,
 VertexArray path(PrimitiveType::LineStrip);
 
 //Flight information
-float x = 0, x_old, y = 0.0000000000000000001, y_old,
-      vx = 0, vy = 0, v,
-      a_thrust = FORCE/MASS, ax, ay, a,
-      dt = 1.f/60.f, t = 0,
-      launch_angle = 80 * PI / 180;
+float dt = 1.f/60.f, t = 0, thrust_time = 1.5,         //Time
+      x = 0, y = 0.0000000000000000001,                //Position
+      vx = 0, vy = 0, v,                               //Velocity
+      a_thrust, ax, ay, a,                             //Acceleration
+
+      force = 400, mass_rocket = 10, mass_fuel = 10,   //Rocket
+      fuel_per_second = mass_fuel / thrust_time,
+      launch_angle = 80 * PI / 180;                     //Angle
 
 //Variables for calculate_bounds function
 float height1, height_max, distance1, distance_max,
@@ -92,15 +108,19 @@ void simulator(RenderWindow& window)
     Text time(font);
     time.setCharacterSize(40);
     time.setPosition({1500.f, 50.f});
+
     Text distance(font);
     distance.setCharacterSize(40);
     distance.setPosition({1500.f, 110.f});
+
     Text height(font);
     height.setCharacterSize(40);
     height.setPosition({1500.f, 170.f});
+
     Text velocity(font);
     velocity.setCharacterSize(40);
     velocity.setPosition({1500.f, 230.f});
+
     Text acceleration(font);
     acceleration.setCharacterSize(40);
     acceleration.setPosition({1500.f, 290.f});
@@ -108,8 +128,10 @@ void simulator(RenderWindow& window)
     //Graph lines info
     RectangleShape x_axis({X_AXIS_LENGTH, 5.f});
     x_axis.setPosition({ORIGIN - 5.f, 1080.f - ORIGIN});
+
     RectangleShape y_axis({5.f, -Y_AXIS_LENGTH});
     y_axis.setPosition({ORIGIN - 5.f, 1085.f - ORIGIN});
+
     RectangleShape ticks({});
 
     //Arrow information
@@ -131,26 +153,32 @@ void simulator(RenderWindow& window)
                 window.close();
         }
 
-        if (t <= time_ground + THRUST_TIME)
+        if (t <= time_ground)
         {
+        //Update position
+        update_numbers();
+
+        //Clear screen
+        window.clear(Color::Black);
+        
         //Draw Screen
-        window.clear(Color::Black); //Clear screen
         draw_graph(window, x_axis, y_axis, ticks);
         draw_state(window, time, distance, height, velocity, acceleration);
         draw_path(window);
         draw_arrows(window);
 
-        //Update position
-        update_numbers();
-
         //Display new
         window.display();
         }
-        else //Keep updating screen
+        else //Stop and keep updating screen
         {
+            //Stop graph
             y = vx = vy = ax = ay = 0;
-            //Draw Screen
+
+            //Clear screen
             window.clear(Color::Black);
+
+            //Draw Screen
             draw_graph(window, x_axis, y_axis, ticks);
             draw_path(window);
             draw_state(window, time, distance, height, velocity, acceleration);
@@ -161,53 +189,128 @@ void simulator(RenderWindow& window)
     }
 }
 
+float acceleration_y(float t)
+{
+    if (t <= thrust_time)
+    {
+        return GRAVITY + force / (mass_rocket + mass_fuel - fuel_per_second * t) * sin(launch_angle);
+    }
+    else
+    {
+        return GRAVITY;
+    }
+
+}
+
+float velocity_y(float t)
+{
+    if (t <= thrust_time)
+    {
+        return GRAVITY * t - (force / fuel_per_second) * std::log(mass_rocket + mass_fuel - fuel_per_second * t) * sin(launch_angle)
+                + (force / fuel_per_second) * std::log(mass_rocket + mass_fuel) * sin(launch_angle);                                               //Constant
+    }
+    else
+    {
+        return velocity_y(thrust_time) + GRAVITY * (t - thrust_time);
+    }
+
+}
+
+float position_y(float t)
+{
+    if (t <= thrust_time)
+    {
+        return 0.5f * GRAVITY * t * t - (force / fuel_per_second) * sin(launch_angle) * 
+                 (std::log(mass_rocket + mass_fuel - fuel_per_second * t) * t
+                  - t - (mass_rocket + mass_fuel) / fuel_per_second * std::log(mass_rocket + mass_fuel - fuel_per_second * t))
+                 + (force / fuel_per_second) * std::log(mass_rocket + mass_fuel) * sin(launch_angle) * t                                            //Constant * t
+                 - (force / fuel_per_second) * sin(launch_angle) * (mass_rocket + mass_fuel) / fuel_per_second * std::log(mass_rocket + mass_fuel); //Constant
+    }
+    else
+    {
+        return position_y(thrust_time)
+            + 0.5f * GRAVITY * (t - thrust_time) * (t - thrust_time)
+            + velocity_y(thrust_time) * (t - thrust_time);
+    }
+}
+
+float acceleration_x(float t)
+{
+    if (t <= thrust_time)
+    {
+        return force / (mass_rocket + mass_fuel - fuel_per_second * t) * cos(launch_angle);
+    }
+    else
+    {
+        return 0.f;
+    }
+}
+
+float velocity_x(float t)
+{
+    if (t <= thrust_time)
+    {
+        return  - (force / fuel_per_second) * std::log(mass_rocket + mass_fuel - fuel_per_second * t) * cos(launch_angle)
+                + (force / fuel_per_second) * std::log(mass_rocket + mass_fuel) * cos(launch_angle);                                               //Constant
+    }
+    else
+    {
+        return velocity_x(thrust_time);
+    } 
+}
+
+float position_x(float t)
+{
+    if (t <= thrust_time)
+    {
+        return  - (force / fuel_per_second) * cos(launch_angle) * 
+                 (std::log(mass_rocket + mass_fuel - fuel_per_second * t) * t
+                  - t - (mass_rocket + mass_fuel) / fuel_per_second * std::log(mass_rocket + mass_fuel - fuel_per_second * t))
+                 + (force / fuel_per_second) * std::log(mass_rocket + mass_fuel) * cos(launch_angle) * t                                            //Constant * t
+                 - (force / fuel_per_second) * cos(launch_angle) * (mass_rocket + mass_fuel) / fuel_per_second * std::log(mass_rocket + mass_fuel); //Constant
+    }
+    else
+    {
+        return position_x(thrust_time)
+            + velocity_x(thrust_time) * (t - thrust_time);
+    }
+}
+
 void calculate_bounds()
 {
-    //Time of peak for freefall portion (with t=0 when booster stops)
-    time_peak = -(a_thrust * sin(launch_angle) * THRUST_TIME + GRAVITY * THRUST_TIME) / GRAVITY;
+    //Time of peak
+    time_peak = - velocity_y(thrust_time) / GRAVITY + thrust_time;
 
-    //Height and velocity at booster off, then max height
-    height1 = 0.5f * (a_thrust * sin(launch_angle) + GRAVITY) * THRUST_TIME * THRUST_TIME;
-    velocity1y = a_thrust * sin(launch_angle) * THRUST_TIME + GRAVITY * THRUST_TIME;
-    height_max = 0.5f * GRAVITY * time_peak * time_peak
-                + velocity1y * time_peak
-                + height1;
+    //Max height
+    height_max = position_y(time_peak);
 
-    //Time of ground for freefall portion (with t=0 when booster stops)
-    time_ground = (-velocity1y - sqrt(velocity1y * velocity1y - 2.f * GRAVITY * height1)) / (2.f * 0.5f * GRAVITY);
+    //Time rocket hits ground
+    time_ground = (-velocity_y(thrust_time) -
+                sqrt(velocity_y(thrust_time) * velocity_y(thrust_time) - 2.f * GRAVITY * position_y(thrust_time)))
+                / (GRAVITY) + thrust_time;
 
-    //Distance and velocity at booster off, then max distance
-    distance1 = 0.5f * a_thrust * cos(launch_angle) * THRUST_TIME * THRUST_TIME;
-    velocity1x = a_thrust * cos(launch_angle) * THRUST_TIME;
-    distance_max = distance1 + velocity1x * time_ground;
+    //Max distance
+    distance_max = position_x(time_ground);
+
+    //Velocity after boosters stop for velocity arrow scale
+    velocity1x = velocity_x(thrust_time);
+    velocity1y = velocity_y(thrust_time);
 }
 
 void update_numbers()
 {
-    //New time
+    //New time, mass, and acceleration
     t += dt;
 
-    //Save old positions
-    x_old = x;
-    y_old = y;
-
     //Get current acceleration, velocity, and position
-    if (t < THRUST_TIME)
-    {
-        ay = a_thrust * sin(launch_angle) + GRAVITY;
-        ax = a_thrust * cos(launch_angle);
-    }
-    else
-    {
-        ay = GRAVITY;
-        ax = 0;
-    }
+    ax = acceleration_x(t);
+    ay = acceleration_y(t);
 
-    vx += ax * dt;
-    vy += ay * dt;
+    vx = velocity_x(t);
+    vy = velocity_y(t);
 
-    y += vy * dt;
-    x += vx * dt;
+    x = position_x(t);
+    y = position_y(t);
 
     //Velocity and acceleration vector magnitudes
     v = sqrt(vx * vx + vy * vy);
@@ -216,16 +319,18 @@ void update_numbers()
 
 void draw_path(RenderWindow& window)
 {
-    if (t <= THRUST_TIME)
+    if (t <= time_ground)
+    {
+        if (t <= thrust_time) //Green during boosters
         {
             path.append(Vertex{{x * pixels_per_meter + ORIGIN, 1080.f - y * pixels_per_meter - ORIGIN}, Color::Green});
-            window.draw(path);
         }
-        if (t > THRUST_TIME)
+        else //Red during freefall
         {
             path.append(Vertex{{x * pixels_per_meter + ORIGIN, 1080.f - y * pixels_per_meter - ORIGIN}, Color::Red});
-            window.draw(path);
         }
+    }
+    window.draw(path);
 }
 
 void draw_graph(RenderWindow& window, RectangleShape& x_axis, RectangleShape& y_axis, RectangleShape& ticks)
@@ -278,12 +383,12 @@ void draw_arrows(RenderWindow& window)
     
     //Make arrow bounds
     v_arrow[0].position = {x * pixels_per_meter + ORIGIN, 1080.f - y * pixels_per_meter - ORIGIN};
-    v_arrow[1].position = {(x + arrow_length_v * vx / v) * pixels_per_meter + ORIGIN,
-                            1080.f - (y + arrow_length_v * vy / v) * pixels_per_meter - ORIGIN};
+    v_arrow[1].position = {x * pixels_per_meter  + arrow_length_v * vx / v + ORIGIN,
+                            1080.f - y * pixels_per_meter - arrow_length_v * vy / v - ORIGIN};
 
     a_arrow[0].position = {x * pixels_per_meter + ORIGIN, 1080.f - y * pixels_per_meter - ORIGIN};
-    a_arrow[1].position = {(x + arrow_length_a * ax / a) * pixels_per_meter + ORIGIN,
-                            1080.f - (y + arrow_length_a * ay / a) * pixels_per_meter - ORIGIN};
+    a_arrow[1].position = {x * pixels_per_meter + arrow_length_a * ax / a + ORIGIN,
+                            1080.f - y * pixels_per_meter - arrow_length_a * ay / a - ORIGIN};
 
     //Draw vectors
     window.draw(v_arrow, 2, PrimitiveType::Lines);
